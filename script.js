7829d787c4d7b2507026d330fb6453b6d6a7d4ea
cbmosley <cbmosley@ncsu.edu>
2022-07-27 14:43:33 -0400

    Calculator computes in display and catches impossible calculations with error message

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 // create event listeners
 // multiplication function
-let calcdisplay = document.getElementById("calcdisplay")
+let calcdisplay = document.getElementById("calcdisplay");
 
 let calcbuttons = Array.from(document.getElementsByClassName("button"));
 
-//console.log(calcbuttons)
-calcbuttons.map ( button => {
-    button.addEventListener("click") {
-        
-    }
+
+calcbuttons.map( button => {
+    button.addEventListener("click", (e) => {
+        switch(e.target.innerText){
+            case "C":
+                calcdisplay.innerText = " ";
+                break;
+            case "←":
+                if(calcdisplay.innerText){
+                    calcdisplay.innerText = calcdisplay.innerText.slice(0, -1);
+                }
+                break;
+            case "=":
+                try{calcdisplay.innerText = eval(calcdisplay.innerText);
+                } catch {
+                    calcdisplay.innerText = "Can't Compute";
+                }
+                break;
+            default:
+                calcdisplay.innerText += e.target.innerText;
+        }
+
+    });
+});
