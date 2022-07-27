1ebe48a55b4a3e79094692f09e6bf0f2135a3a47
cbmosley <cbmosley@ncsu.edu>
2022-07-27 00:23:11 -0400

    format and display change.

---
+++ script.js
@@ -0,0 +1,11 @@
+// create event listeners
+// multiplication function
+let calcdisplay = document.getElementById("calcdisplay")
+
+let calcbuttons = Array.from(document.getElementsByClassName("button"));
+
+//console.log(calcbuttons)
+calcbuttons.map ( button => {
+    button.addEventListener("click") {
+        
+    }
